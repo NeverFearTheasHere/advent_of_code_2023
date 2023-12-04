9ba41e7a258ed6269d4c53d097ef8df15ca2a4e4
Thea Lanherne <thea.lanherne@ghyston.com>
2023-12-04 11:56:52 +0000

    day 4 part 2

--- day04.py
+++ day04.py
@@ -7,8 +7,8 @@ def calculateTotalScratchcardPoints(scratchcardInputs):
     for scratchcard in scratchcardInputs:
         numbersSections = scratchcard.split(': ')[1]
         [winningNumbersSection, myNumbersSection] = numbersSections.split('| ')
-        winningNumbers = [int(x) for x in re.findall('\d+', winningNumbersSection)]
-        myNumbers = [int(x) for x in re.findall('\d+', myNumbersSection)]
+        winningNumbers = [int(x) for x in re.findall(r'\d+', winningNumbersSection)]
+        myNumbers = [int(x) for x in re.findall(r'\d+', myNumbersSection)]
 
         scratchcardScore = 0
         for winningNumber in winningNumbers:
@@ -23,8 +23,34 @@ def calculateTotalScratchcardPoints(scratchcardInputs):
     return totalScratchcardPoints
 
 
+def calculateNumberOfScratchcards(scratchcardInputs):
+    countOfscratchcardsCopiesByCardNumber = { }
+
+    for scratchcard in scratchcardInputs:
+        [cardLabel, numbersSections] = scratchcard.split(r': ')
+        cardNumber = int(re.search(r'\d+', cardLabel).group())
+        [winningNumbersSection, myNumbersSection] = numbersSections.split(r'| ')
+        winningNumbers = [int(x) for x in re.findall(r'\d+', winningNumbersSection)]
+        myNumbers = [int(x) for x in re.findall(r'\d+', myNumbersSection)]
+        
+        additionalCardNumber = cardNumber
+        for winningNumber in winningNumbers:
+            if winningNumber in myNumbers:
+                additionalCardNumber = additionalCardNumber + 1
+                if additionalCardNumber in countOfscratchcardsCopiesByCardNumber:
+                    countOfscratchcardsCopiesByCardNumber[additionalCardNumber] += countOfscratchcardsCopiesByCardNumber[cardNumber] + 1
+                else:
+                    if cardNumber in countOfscratchcardsCopiesByCardNumber:
+                        countOfscratchcardsCopiesByCardNumber[additionalCardNumber] = countOfscratchcardsCopiesByCardNumber[cardNumber] + 1
+                    else:
+                        countOfscratchcardsCopiesByCardNumber[additionalCardNumber] = 1
+    
+    numberOfCopies = sum(countOfscratchcardsCopiesByCardNumber.values())
+    return len(scratchcardInputs) + numberOfCopies
+
+
 if __name__ == "__main__":
     with open('day04_input.txt') as scratchcardFile:
         scratchcardInputs = scratchcardFile.read().splitlines()
-        answer = calculateTotalScratchcardPoints(scratchcardInputs)
+        answer = calculateNumberOfScratchcards(scratchcardInputs)
         print(answer)
--- day04_test.py
+++ day04_test.py
@@ -2,8 +2,7 @@ import day04
 import unittest
 
 class Test_day04(unittest.TestCase):
-    def test_part1(self):
-        exampleInput = [
+    exampleInput = [
             'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
             'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
             'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
@@ -11,7 +10,12 @@ class Test_day04(unittest.TestCase):
             'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
             'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'
         ]
-        self.assertEqual(day04.calculateTotalScratchcardPoints(exampleInput), 13)
+
+    def test_part1(self):   
+        self.assertEqual(day04.calculateTotalScratchcardPoints(self.exampleInput), 13)
+
+    def test_part2(self):
+        self.assertEqual(day04.calculateNumberOfScratchcards(self.exampleInput), 30)
 
 
 if __name__ == '__main__':
